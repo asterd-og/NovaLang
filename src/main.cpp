0681bae7f634399d7655bb6967aaf7cc5de63ebc
astrido <connect5554@outlook.com>
2023-10-23 16:53:24 -0300

    Changes

--- src/main.cpp
+++ src/main.cpp
@@ -64,6 +64,8 @@ int main(int argc, char** argv) {
 
     system(stringf("clang tmp.c -o %s -Wno-implicit-int -Wno-main-return-type -Wno-implicit-function-declaration", argv[2]).c_str());
 
+    remove("tmp.c");
+
     //Eval evaluator;
     //evaluator.eval(parser.ast);
 }
